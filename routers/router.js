ae07c21d99a8122504ff3cc094e50df10a1bbdbb
Limstar12 <phji0326@gmail.com>
2022-02-28 18:28:42 +0900

    리액트 추가

---
+++ routers/router.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res){
+    res.send("/user라고 하면 내가 나옴")
+})
+
+router.get("/company", function(req, res){
+    res.send("/user/company라고 하면 내가 나옴")
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express = require("express");
+var projectlink = require("./routers/router");
 var app = express();
 
+
+app.get('/', function(a,b){
+    b.send('나온다');
+});
+
+app.get('/me', function(a,b){
+    b.send('나의 소개');
+});
+app.get('/portfolio', function(a,b){
+    b.send('작품들');
+});
+app.get('/preinterview', function(a,b){
+    b.send('사전면접');
+});
+app.get('/contact', function(req,res){
+    res.send('면접제안');
+});
+
+app.use("/user", projectlink);
+
 app.listen(5000, function(){
-  
+  console.log('ㅇㅇㅇㅇㅇㅇㅇ');
 });
